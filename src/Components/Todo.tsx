5a09ccbd11360d1db84e540bc3c850fa67c2a7ce
Sejal Singh <sejal@geekyants.com>
2023-04-26 14:27:51 +0530

    todo list typescript

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import Context from "./context";
-
 import TodoInput from "./TodoInput";
 
 
 export type Filter = "All" | "Active" | "Completed";
-
 const App: React.FC = () => {
 
   return (
